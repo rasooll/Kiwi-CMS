5f1c2bce43466d79d085b715f9c5bc5f72dcba9c
Rasool Safari <safari@rasooll.com>
2018-05-24 19:51:35 +0430

    Fix list filter for blog posts

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ class BlogAdmin(admin.ModelAdmin):
     exclude = ['posted']
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'author', 'published_date', 'category', 'tags')
-    list_filter = ('published_date',)
+    list_filter = ('published_date','category',)
     search_fields = ('title', 'slug', 'tags')
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -16,6 +16,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'post', 'text', 'date')
     list_filter = ('date',)
     search_fields = ('text', 'post')
+    raw_id_fields = ('post',)
 
 admin.site.register(Post, BlogAdmin)
 admin.site.register(Category, CategoryAdmin)
