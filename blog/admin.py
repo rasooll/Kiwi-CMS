fbdc673091ed6c65c644a16d50c7dacf91fcaba2
Rasool Safari <safari@rasooll.com>
2018-05-24 19:22:32 +0430

    Config model in admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,10 +4,19 @@ from blog.models import Post, Category, Comment
 class BlogAdmin(admin.ModelAdmin):
     exclude = ['posted']
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'published_date', 'category', 'tags')
+    list_filter = ('published_date',)
+    search_fields = ('title', 'slug', 'tags')
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'text', 'date')
+    list_filter = ('date',)
+    search_fields = ('text', 'post')
 
 admin.site.register(Post, BlogAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
