7b00acce4b24754c514880a03abdc9608484cac1
Rasool Safari <safari@rasooll.com>
2018-05-24 18:49:18 +0430

    Default script language to Persian.

--- Kiwi/settings.py
+++ Kiwi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jalali',
     'ckeditor',
     'ckeditor_uploader',
     'tagging',
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'Kiwi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'Kiwi/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-IR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,8 @@ class Category(models.Model):
     """
     Model for Category
     """
-    title = models.CharField(max_length=100, db_index=True)
-    slug = models.SlugField(max_length=100, db_index=True)
+    title = models.CharField(max_length=100, db_index=True, verbose_name='عنوان')
+    slug = models.SlugField(max_length=100, db_index=True, help_text='نام انگلیسی برای استفاده در لینک این دسته بندی')
 
     def __str__(self):
         return self.title
@@ -20,19 +20,23 @@ class Category(models.Model):
     def get_absolute_url(self):
         return ('view_blog_category', None, { 'slug': self.slug })
 
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
 class Post(models.Model):
     """
     Model for Posts
     """
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    author=models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='نویسنده')
+    title = models.CharField(max_length=100, unique=True, verbose_name='عنوان')
+    slug = models.SlugField(max_length=100, unique=True, help_text='نام انگلیسی برای استفاده در لینک این نوشته')
     published_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    description = models.TextField()
-    content = RichTextUploadingField(config_name='awesome_ckeditor')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    tags = TagField()
+    description = models.TextField(verbose_name='متن معرفی', help_text='توضیحات مختصری درباره‌ی این نوشته')
+    content = RichTextUploadingField(config_name='awesome_ckeditor', verbose_name='متن اصلی', help_text='این متن در صفحه اصلی نوشته نمایش داده می‌شود')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='دسته بندی')
+    tags = TagField(verbose_name='برچسب‌ها', help_text='با استفاده از کاما (,) آنها را از یکدیگر جدا نمایید')
 
     def __str__(self):
         return self.title
@@ -41,15 +45,23 @@ class Post(models.Model):
     def get_absolute_url(self):
         return ('view_blog_post', None, { 'slug': self.slug })
 
+    class Meta:
+        verbose_name = 'نوشته'
+        verbose_name_plural = 'نوشته ها'
+
 class Comment(models.Model):
     """
     Model for Comments for Posts.
     """
-    post=models.ForeignKey(Post, on_delete=models.CASCADE)
-    name=models.CharField(max_length=100)
-    email=models.EmailField()
-    text=models.TextField()
+    post=models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='نوشته')
+    name=models.CharField(max_length=100, verbose_name='نام شما')
+    email=models.EmailField(verbose_name='آدرس ایمیل')
+    text=models.TextField(verbose_name='متن دیدگاه')
     date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "{} --- {}".format(self.post.title, self.text)
+
+    class Meta:
+        verbose_name = 'دیدگاه'
+        verbose_name_plural = 'دیدگاه ها'
