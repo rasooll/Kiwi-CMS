606c73cafd26fe9553e9a70f5c685bcc00ed0ac9
Rasool Safari <safari@rasooll.com>
2018-07-01 13:48:43 +0430

    No need to user extra info on this project.

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Profile
 
 # Register your models here.
-
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('get_username','get_full_name')
-
-admin.site.register(Profile, ProfileAdmin)
